87c2063e2aa948eeda1d9c754d1d37b434a6e673
jpaine126 <j.paine126@gmail.com>
2024-06-22 14:39:25 -0400

    added name to model, and made id self-calculating

--- tutorial/readings/models.py
+++ tutorial/readings/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
-# Create your models here.
+import uuid
 
 
 class Readings(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    id = models.CharField(max_length=100, primary_key=True, blank=True)
+    location = models.CharField(max_length=20)
     time = models.DateTimeField()
     temperature = models.FloatField()
     humidity = models.FloatField()
+
+    @property
+    def get_id(self):
+        return str(uuid.uuid4())
+
+    def save(self, *args, **kwargs):
+        self.id = self.get_id
+        super(Readings, self).save(*args, **kwargs)
 
     def __str__(self):
         return (
